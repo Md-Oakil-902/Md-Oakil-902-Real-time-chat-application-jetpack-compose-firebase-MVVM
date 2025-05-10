81075e71d89cebd76335a4dab6f03c846f9ab106
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-05-10 22:05:56 +0600

    making channel

--- app/src/main/java/com/oakil/realtimechatapplication/features/Home/HomeScreen.kt
+++ app/src/main/java/com/oakil/realtimechatapplication/features/Home/HomeScreen.kt
@@ -1,19 +1,26 @@
 package com.oakil.realtimechatapplication.features.Home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+
 
 
 @Composable
@@ -32,7 +39,19 @@ fun HomeScreen(navController: NavController) {
                 items(channels.value){
                     channel ->
                     Column{
-                        Text(text = channel.name, color = Color.Red)
+                        Text(text = channel.name,
+                            modifier = Modifier.fillMaxWidth()
+                                .padding(8.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(Color.Red.copy(alpha = 0.3f))
+                                .clickable {
+                                    //navController.navigate("chat/${channel.id}")
+                                    navController.navigate("signup")
+                                }
+                                .padding(16.dp)
+
+
+                        )
                     }
                 }
 
